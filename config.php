0c27a0392879c8d548011ddaa1f0f09962869e8c
Takumi Kondo <info@loglog.xyz>
2019-02-24 13:20:28 +0900

    DB Pass changed

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 $db['host'] = "localhost";  // DBサーバのURL
 $db['user'] = "root";  // ユーザー名
-$db['pass'] = "";  // ユーザー名のパスワード
+$db['pass'] = "root";  // ユーザー名のパスワード
 $db['dbname'] = "boards";  // データベース名
 
 date_default_timezone_set('Asia/Tokyo');
